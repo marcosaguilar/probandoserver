9e2b42aca3e9ec7f2788a2c81d1d97a844e8ed37
marcos aguilar <marcosaguilarn91@gmail.com>
2017-03-31 19:05:12 -0400

    prueba1

---
+++ probandoserver/views.py
@@ -0,0 +1,24 @@
+from django.template import RequestContext
+from django.shortcuts import render
+from forms import LoginForm
+from django.contrib.auth import authenticate , login
+
+def login_page(request):
+    message=None
+    if request.method == "POST":
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            username=request.POST['username']
+            password=request.POST['pasword']
+            user=authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request,user)
+                    message="te has identificado"
+                else:
+                    message="incorrecto"
+        else:
+            message="nombre de usuario o contrasena incorrecta"
+    else:
+        form=LoginForm()
+    return render(request,'login.html',{'message':message, 'form':form})
