5a332596776649c73a9e7ff050a4cd6e0ad046c0
marcos aguilar <marcosaguilarn91@gmail.com>
2017-05-20 05:40:12 -0400

    agrega mas test unitarios

--- recurso/tests.py
+++ recurso/tests.py
@@ -14,9 +14,22 @@ class MantenimientoTestCase(TestCase):
         mantenimiento1 = Mantenimiento.objects.create(estado=estadomantenimiento1, tipo=tipomantenimiento1,
                                                       descripcion="cambio de pantalla", fecha_inicio="2018-02-01 02:10:00+00:00",
                                                       fecha_fin="2019-02-02 03:10:00+00:00", cod_recurso=recurso1.id)
+        recurso2 = recurso.objects.create(nombre="Proyector789", estado=estadorecurso1, tipo=tiporecurso1, mantenimiento=mantenimiento1)
 
 
     def test_Mantenimiento_estado_nombre(self):
         estadomantenimiento1 = estadoMantenimiento.objects.create(nombre="A realizar")
         mantenimiento1 = Mantenimiento.objects.get(descripcion="cambio de pantalla")
         self.assertEqual(mantenimiento1.estado.nombre, estadomantenimiento1.nombre)
+
+
+    def test_Mantenimiento_fecha_fin(self):
+        mantenimiento1 = Mantenimiento.objects.get(descripcion="cambio de pantalla")
+        self.assertEqual(mantenimiento1.fecha_fin.__str__(), "2019-02-02 03:10:00+00:00")
+
+
+    def test_recurso_Mantenimiento(self):
+        mantenimiento1 = Mantenimiento.objects.get(descripcion="cambio de pantalla")
+        recurso2 = recurso.objects.get(nombre="Proyector789")
+        self.assertEqual(mantenimiento1.descripcion, recurso2.mantenimiento.descripcion)
+
