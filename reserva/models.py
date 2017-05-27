982d2fae1cefc95306d4164dbc65f36e9b0c91f9
marcos aguilar <marcosaguilarn91@gmail.com>
2017-05-27 18:22:59 -0400

    lista reserva

--- reserva/models.py
+++ reserva/models.py
@@ -12,14 +12,25 @@ class estadoReserva(models.Model):
         return '{}'.format(self.estado)
 
 
+class listaReserva(models.Model):
+    """este modelo contiene los atributos de la lista de reserva"""
+    fecha = models.TextField(max_length=50) #solo el dia
+    recurso = models.ForeignKey(recurso, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __unicode__(self):
+        return '{}'.format(self.id)
+
+
 class reserva(models.Model):
     """este modelo contiene los atributos de una reserva"""
-    recurso = models.ForeignKey(recurso, null=True, blank=True, on_delete=models.CASCADE)
+    recurso = models.ForeignKey(recurso, on_delete=models.CASCADE)
     tipo_recurso = models.ForeignKey(Tipo_de_recurso, null=True, blank=True, on_delete=models.CASCADE)
     usuario = models.ForeignKey(usuario, null=True, blank=True, on_delete=models.CASCADE)
     fecha_inicio = models.TextField(max_length=50)
     fecha_fin = models.TextField(max_length=50)
     estado_reserva = models.ForeignKey(estadoReserva, null=True, blank=True, on_delete=models.CASCADE)
+    lista_reserva = models.ForeignKey(listaReserva, null=True, blank=True, on_delete=models.CASCADE)
+    gano_reserva = models.NullBooleanField()
 
     def __unicode__(self):
         return '{}'.format(self.id)
--- reserva/views.py
+++ reserva/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import permission_required
 from forms import CrearReservaForm, EditarReservaForm
-from models import reserva, estadoReserva
+from models import reserva, estadoReserva, listaReserva
+from datetime import datetime, timedelta
+from recurso.models import recurso
 
 # Create your views here.
 
@@ -13,12 +15,62 @@ def crearReserva_view(request):
     if request.method == 'POST':
         form = CrearReservaForm(request.POST)
         if form.is_valid():
+            nosepudoreservar = False
             form = form.save(commit=False)
             form.tipo_recurso = form.recurso.tipo
             form.usuario = request.user
             form.estado_reserva = estadoReserva.objects.get(estado="A confirmar")
-            form.save()
-            return redirect('/reserva/listarreserva')
+            #ver si entra en lista o es directa
+            #si es menos de 48 hs
+            fechainicio = datetime.strptime(form.fecha_inicio, '%Y-%m-%d %H:%M') #convierte a tipo datetime
+            fechafinal = datetime.strptime(form.fecha_fin, '%Y-%m-%d %H:%M')
+            if(fechainicio.date().__str__() <= (datetime.now().date()+timedelta(days=2)).__str__()):
+                #busca lista de reserva de ese recurso y fecha igual
+                listaencontrada = False
+                for listareserva in listaReserva.objects.all():
+                    if (listareserva.recurso == form.recurso and fechainicio.date().__str__() == listareserva.fecha):
+                        listaencontrada = True
+                        break
+                #si encuentra busca entre las reservas ganadoras y compara sus horas
+                if(listaencontrada):
+                    puedereservar = True
+                    for reserva1 in reserva.objects.filter(lista_reserva=listareserva):
+                        if(reserva1.gano_reserva):
+                            if(reserva1.fecha_inicio <= fechainicio.__str__() and
+                            reserva1.fecha_fin >= fechainicio.__str__() or
+                            reserva1.fecha_inicio <= fechafinal.__str__() and
+                            reserva1.fecha_fin >= fechafinal.__str__()):
+                                puedereservar = False
+                                break
+                    if (puedereservar):
+                        form.lista_reserva = listareserva
+                        form.gano_reserva = True
+                    else:
+                        nosepudoreservar = True
+                #si no crea una lista y reserva directamente
+                else:
+                    form.lista_reserva = listaReserva.objects.create(fecha=fechainicio.date().__str__(), recurso=form.recurso)
+                    form.gano_reserva = True
+            #si es mas de 48 hs
+            else:
+                listaencontrada = False
+                #busca lista de reserva de ese recurso y fecha igual
+                for listareserva in listaReserva.objects.all():
+                    if (listareserva.recurso == form.recurso and fechainicio.date().__str__() == listareserva.fecha):
+                        listaencontrada = True
+                        break
+                #si encuentra entra en la lista
+                if (listaencontrada):
+                    form.lista_reserva = listareserva
+                #si no crea una lista reserva
+                else:
+                    form.lista_reserva = listaReserva.objects.create(fecha=fechainicio.date().__str__(), recurso=form.recurso)
+            if (not nosepudoreservar):
+                form.save()
+                return redirect('/reserva/listarreserva')
+            else:
+                #agregar pagina con mensaje de que no se pudo crear
+                return redirect('/reserva/listarreserva')
     else:
         form = CrearReservaForm()
 
@@ -45,4 +97,11 @@ def editarReserva_view(request, id_reserva):
             form.save()
         return redirect('reserva:listar_reserva')
     return render(request, 'reserva/crearReserva_form.html', {'form': form})
+
+
+def listarListaReserva_view(request):
+    """despliega una lista de listas de reservas registradas en el sistema"""
+    lista1 = listaReserva.objects.all().order_by('id')
+    contexto1 = {'listas': lista1}
+    return render(request,'reserva/listarListaReserva_form.html', contexto1)
 
